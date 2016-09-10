bd7c9dfacc3749b2e09b124f68fea01b95d44671
Jim Kirkbride <james.kirkbride@gmail.com>
2016-09-10 13:51:47 -0400

    Refactoring.

--- spec/driving_history_calculator_spec.rb
+++ spec/driving_history_calculator_spec.rb
@@ -2,22 +2,13 @@ require 'stringio'
 
 describe DrivingHistoryCalculator do
 
-    it 'outputs an entry for a driver with no records' do
-        driving_records = ['Driver Bob']
-        calculator = DrivingHistoryCalculator.new
-
-        report_lines = calculator.calc(driving_records)
-
-        expect(report_lines.first).to eq('Bob: 0 miles')
-    end
+    let(:calculator) { DrivingHistoryCalculator.new }
 
     it 'outputs entries for drivers with no record' do
       driving_records = [
         'Driver Dan',
         'Driver Steve'
       ]
-
-      calculator = DrivingHistoryCalculator.new
 
       report_lines = calculator.calc(driving_records)
 
