b4fdd5d828cfed91cfa9216ddd2c572650d7569a
Jim Kirkbride <james.kirkbride@gmail.com>
2016-09-10 18:35:53 -0400

    Refactoring.

--- lib/driving_history_calculator.rb
+++ lib/driving_history_calculator.rb
@@ -4,6 +4,14 @@ class DrivingHistoryCalculator
       # TODO: Handle duplicate Drivers
       # TODO: Handle invalid command entries
       # TODO: Handle invalidly formatted commands
+      driver_names = collect_drivers(lines)
+      trips = collect_trips(lines)
+      create_reportable_trips(driver_names, trips)
+    end
+
+    private
+
+    def collect_drivers(lines)
       driver_names = lines.select{ |line|
         line.start_with? 'Driver'
       }.map{ |driver|
@@ -11,6 +19,10 @@ class DrivingHistoryCalculator
         {driver_tokens[1] => 0}
       }.reduce({}, :merge)
 
+      driver_names
+    end
+
+    def collect_trips(lines)
       trips = lines.select{ |line|
         line.start_with? 'Trip'
       }.map {|trip|
@@ -18,7 +30,11 @@ class DrivingHistoryCalculator
         {trip_tokens[1] => trip_tokens[4].to_f.round.to_i}
       }.reduce({}, :merge)
 
-      reported_trips = driver_names.merge(trips)
+      trips
+    end
+
+    def create_reportable_trips(drivers, trips)
+      reported_trips = drivers.merge(trips)
         .map {|key, value|
           "#{key}: #{value} miles"
         }
