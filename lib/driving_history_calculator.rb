f28d6f6114c703a43a5533b19078f118f267b271
Jim Kirkbride <james.kirkbride@gmail.com>
2016-09-10 18:47:26 -0400

    Refactoring.

--- lib/driving_history_calculator.rb
+++ lib/driving_history_calculator.rb
@@ -11,12 +11,18 @@ class DrivingHistoryCalculator
 
     private
 
+    RECORD_SPLIT_PATTERN = ' '
+    COMMAND_DRIVER = 'Driver'
+    COMMAND_TRIP = 'Trip'
+    DRIVER_NAME_INDEX = 1
+    TRIP_DISTANCE_INDEX = 4
+
     def collect_drivers(lines)
       driver_names = lines.select{ |line|
-        line.start_with? 'Driver'
+        line.start_with? COMMAND_DRIVER
       }.map{ |driver|
-        driver_tokens = driver.split(' ')
-        {driver_tokens[1] => 0}
+        driver_tokens = driver.split(RECORD_SPLIT_PATTERN)
+        {driver_tokens[DRIVER_NAME_INDEX] => 0}
       }.reduce({}, :merge)
 
       driver_names
@@ -24,10 +30,10 @@ class DrivingHistoryCalculator
 
     def collect_trips(lines)
       trips = lines.select{ |line|
-        line.start_with? 'Trip'
+        line.start_with? COMMAND_TRIP
       }.map {|trip|
-        trip_tokens = trip.split(' ')
-        {trip_tokens[1] => trip_tokens[4].to_f.round.to_i}
+        trip_tokens = trip.split(RECORD_SPLIT_PATTERN)
+        {trip_tokens[DRIVER_NAME_INDEX] => trip_tokens[TRIP_DISTANCE_INDEX].to_f.round.to_i}
       }.reduce({}, :merge)
 
       trips
