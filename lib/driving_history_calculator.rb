dd0811e5de2e789f6480b7b99c0fbf91a2280f95
Jim Kirkbride <james.kirkbride@gmail.com>
2016-09-10 18:20:23 -0400

    Driving distance for one driver with one trip.

--- lib/driving_history_calculator.rb
+++ lib/driving_history_calculator.rb
@@ -4,10 +4,25 @@ class DrivingHistoryCalculator
       # TODO: Handle duplicate Drivers
       # TODO: Handle invalid command entries
       # TODO: Handle invalidly formatted commands
-      driver_names = lines.collect do|line|
-        line.split(' ')[1] + ': 0 miles'
-      end
-      
-      driver_names
+      driver_names = lines.select{ |line|
+        line.start_with? 'Driver'
+      }.map{ |driver|
+        driver_tokens = driver.split(' ')
+        {driver_tokens[1] => 0}
+      }.reduce({}, :merge)
+
+      trips = lines.select{ |line|
+        line.start_with? 'Trip'
+      }.map {|trip|
+        trip_tokens = trip.split(' ')
+        {trip_tokens[1] => trip_tokens[4].to_f.round.to_i}
+      }.reduce({}, :merge)
+
+      reported_trips = driver_names.merge(trips)
+        .map {|key, value|
+          "#{key}: #{value} miles"
+        }
+
+      reported_trips
     end
 end
--- spec/driving_history_calculator_spec.rb
+++ spec/driving_history_calculator_spec.rb
@@ -12,7 +12,20 @@ describe DrivingHistoryCalculator do
 
       report_lines = calculator.calc(driving_records)
 
+      expect(report_lines.length).to eq(2)
       expect(report_lines).to include('Dan: 0 miles')
       expect(report_lines).to include('Steve: 0 miles')
+    end
+
+    it 'calculates the distance for a single valid trip' do
+      driving_records = [
+        'Driver Dan',
+        'Trip Dan 07:15 07:45 17.3'
+      ]
+
+      report_lines = calculator.calc(driving_records)
+
+      expect(report_lines.length).to eq(1)
+      expect(report_lines[0]).to start_with('Dan: 17 miles')
     end
 end
