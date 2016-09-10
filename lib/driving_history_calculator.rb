db2602ae73d3f9db18d9a74795f49745edef6e62
Jim Kirkbride <james.kirkbride@gmail.com>
2016-09-10 10:32:34 -0400

    Handling multiple drivers with no trip history.  Added TODOs for future edgecases.

--- lib/driving_history_calculator.rb
+++ lib/driving_history_calculator.rb
@@ -1,6 +1,13 @@
 class DrivingHistoryCalculator
 
-    def calc(lines) 
-        ['Bob: 0 miles']
+    def calc(lines)
+      # TODO: Handle duplicate Drivers
+      # TODO: Handle invalid command entries
+      # TODO: Handle invalidly formatted commands
+      driver_names = lines.collect do|line|
+        line.split(' ')[1] + ': 0 miles'
+      end
+      
+      driver_names
     end
 end
--- spec/driving_history_calculator_spec.rb
+++ spec/driving_history_calculator_spec.rb
@@ -5,8 +5,23 @@ describe DrivingHistoryCalculator do
     it 'outputs an entry for a driver with no records' do
         driving_records = ['Driver Bob']
         calculator = DrivingHistoryCalculator.new
-        
+
         report_lines = calculator.calc(driving_records)
+
         expect(report_lines.first).to eq('Bob: 0 miles')
+    end
+
+    it 'outputs entries for drivers with no record' do
+      driving_records = [
+        'Driver Dan',
+        'Driver Steve'
+      ]
+
+      calculator = DrivingHistoryCalculator.new
+
+      report_lines = calculator.calc(driving_records)
+
+      expect(report_lines).to include('Dan: 0 miles')
+      expect(report_lines).to include('Steve: 0 miles')
     end
 end
