a5b055165714e2e8d5f056ae24967c3ab9cb2e72
Jim Kirkbride <james.kirkbride@gmail.com>
2016-09-10 18:40:15 -0400

    Adding coverage for mixed entries including drivers with no trips.

--- spec/driving_history_calculator_spec.rb
+++ spec/driving_history_calculator_spec.rb
@@ -28,4 +28,18 @@ describe DrivingHistoryCalculator do
       expect(report_lines.length).to eq(1)
       expect(report_lines[0]).to start_with('Dan: 17 miles')
     end
+
+    it 'outputs mixed entries including drivers with no trips' do
+      driving_records = [
+        'Driver Dan',
+        'Trip Dan 07:15 07:45 17.3',
+        'Driver Edward'
+      ]
+
+      report_lines = calculator.calc(driving_records)
+
+      expect(report_lines.length).to eq(2)
+      expect(report_lines[0]).to start_with('Dan: 17 miles')
+      expect(report_lines[1]).to eq('Edward: 0 miles')
+    end
 end
