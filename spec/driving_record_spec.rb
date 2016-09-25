088b66ebd5fb8061cebb445594a2971dee7036ea
Jim Kirkbride <james.kirkbride@gmail.com>
2016-09-25 19:08:50 -0400

    Adding coverage for calculating trip distance and minutes

--- spec/driving_record_spec.rb
+++ spec/driving_record_spec.rb
@@ -8,12 +8,52 @@ describe DrivingRecord do
     expect(driving_record.name).to eq(expected_name)
   end
 
-  it 'can calculate the total trip minutes' do
-    driving_record = DrivingRecord.new('short')
-    driving_record.add_record(Trip.new('short', 5.0, '07:15', '07:45'))
-    driving_record.add_record(Trip.new('longer', 10.5, '08:00', '09:00'))
+  describe 'When calculating total trip minutes' do
+    it 'can calculate minutes for a valid trip' do
+      driving_record = DrivingRecord.new('short')
+      driving_record.add_record(Trip.new('short', 5.0, '07:15', '07:45'))
+      driving_record.add_record(Trip.new('longer', 10.5, '08:00', '09:00'))
 
-    expect(driving_record.calc_total_minutes).to eq(90)
+      expect(driving_record.calc_total_minutes).to eq(90)
+    end
+
+    it 'excludes trips with a speed less than 5 mph' do
+      driving_record = DrivingRecord.new('short')
+      driving_record.add_record(Trip.new('short', 2.5, '07:15', '08:15'))
+
+      expect(driving_record.calc_total_minutes).to eq(0)
+    end
+
+    it 'excludes trips with a speed greater than 100mph' do
+      driving_record = DrivingRecord.new('short')
+      driving_record.add_record(Trip.new('short', 1000, '07:15', '08:15'))
+
+      expect(driving_record.calc_total_minutes).to eq(0)
+    end
+  end
+
+  describe 'When calculating total distance' do
+    it 'can calculate distance for a valid trip' do
+      driving_record = DrivingRecord.new('short')
+      driving_record.add_record(Trip.new('short', 5.0, '07:15', '07:45'))
+      driving_record.add_record(Trip.new('longer', 10.5, '08:00', '09:00'))
+
+      expect(driving_record.calc_total_distance).to eq(15.5)
+    end
+
+    it 'excludes trips with a speed less than 5 mph' do
+      driving_record = DrivingRecord.new('short')
+      driving_record.add_record(Trip.new('short', 2.5, '07:15', '08:15'))
+
+      expect(driving_record.calc_total_distance).to eq(0)
+    end
+
+    it 'excludes trips with a speed greater than 100mph' do
+      driving_record = DrivingRecord.new('short')
+      driving_record.add_record(Trip.new('short', 1000, '07:15', '08:15'))
+
+      expect(driving_record.calc_total_distance).to eq(0)
+    end
   end
 
   it 'can calculate the average speed for a single trip' do
